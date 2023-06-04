f90ddee8d09003fcb798dd4c5d26e24bc7c3b124
werik-alves <werikalves@hotmail.com>
2023-06-04 19:43:43 -0300

    Tabela Drop Scrip

---
+++ script.js
@@ -0,0 +1,15 @@
+var row;
+function start (){
+    row  = event.target;
+}
+
+function dragover(){
+    var e = event;
+    e.preventDefault();
+    let children = Array.from(e.target.parentNode.parentNode.children);
+    
+    if(children.indexOf(e.target.parentNode)>children.indexOf(row))
+    e.target.parentNode.after(row);
+        else
+        e.target.parentanode.be
+}
